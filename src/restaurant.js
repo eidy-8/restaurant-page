eebe28af2e5bb1d03edf4181b4232a6516a79cc9
enrique <enriqueonaga@gmail.com>
2023-12-19 19:26:54 -0300

    create the path to load the pages

---
+++ src/restaurant.js
@@ -0,0 +1,8 @@
+const createRestaurantHomePage = () => {
+    const content = document.getElementById("content");
+    const header = document.createElement("header");
+    content.appendChild(header);
+    
+}
+
+export default createRestaurantHomePage;
